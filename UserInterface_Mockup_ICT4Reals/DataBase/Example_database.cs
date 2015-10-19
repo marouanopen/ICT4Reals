d14e4cf23227b76c633bd620e2bf32e12d116aae
kevin1995deman <kevin1995deman@gmail.com>
2015-10-19 11:31:35 +0200

    database get working

--- UserInterface_Mockup_ICT4Reals/DataBase/Example_database.cs
+++ UserInterface_Mockup_ICT4Reals/DataBase/Example_database.cs
@@ -3,31 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserInterface_Mockup_ICT4Reals.DataBase;
 
 namespace UserInterface_Mockup_ICT4Reals.DataBase
 {
-    class Example_database
+    class Example_database : Database
     {
-        /*
-        public List<int> QueryName() //name of ur query
+
+
+        // if u want to use get/do add using UserInterface_Mockup_ICT4Reals.DataBase;
+           public List<string> QueryName() //name of ur query
         {
-            List<int> ret; //result of query will end up in here
-            List<Dictionary<string, object>> QueryX = getQuery("SELECT * FROM event"); //replace your query with te example query, replace 'QueryX' with a clear name.
+            List<string> ret = new List<string>(); //result of query will end up in here
+            List<Dictionary<string, object>> QueryX = getQuery("SELECT naam FROM gebruiker WHERE GebruikerID = 1"); //replace your query with te example query, replace 'QueryX' with a clear name.
             foreach (Dictionary<string, object> results in QueryX) //look for all posseble results in the query result.
             {
-                ret.Add((Convert.ToInt32(results["id"]))); //add each result to the created list, if the list if for a class, u need to add 'new class_name' infront of the convert
+                ret.Add((Convert.ToString(results["naam"]))); //add each result to the created list, if the list if for a class, u need to add 'new class_name' infront of the convert
             }
 
             return ret;     //this will return the list as result from the query.
         }
-        public string QueryName() //name of ur query
-        {
-            string ret; //result of query will end up in here
-            List<Dictionary<string, object>> QueryX = getQuery("SELECT id FROM tram"); //replace your query with te example query, replace 'QueryX' with a clear name.
-            ret = Convert.ToString(QueryX); //add the result of the query to the string
 
-            return ret;     //this will return thes result from the query.
-        }
+        /* 
+         * 
 
         public bool DoQueryExample(user user) // replace user with the data u want to add/ change to the table
         {
