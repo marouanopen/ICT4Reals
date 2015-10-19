ee6bf7b2ed8c695e66eb52bdf927fcd095b79df3
HecticKaluha <s.verhoeve@student.fontys.nl>
2015-10-19 16:32:08 +0200

    mainform setups

--- UserInterface_Mockup_ICT4Reals/MainForm.cs
+++ UserInterface_Mockup_ICT4Reals/MainForm.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserInterface_Mockup_ICT4Reals.AdminSystem;
 
 namespace UserInterface_Mockup_ICT4Reals
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private Administration administration;
+        public MainForm(Administration administration)
         {
+            this.administration = administration;
             InitializeComponent();
+            if(administration.LoggedInUser.RoleId == 1)
+            {
+
+            }
+            if (administration.LoggedInUser.RoleId == 2)
+            {
+
+            }
+            if (administration.LoggedInUser.RoleId == 3)
+            {
+
+            }
+            if (administration.LoggedInUser.RoleId == 4)
+            {
+
+            }
+            if (administration.LoggedInUser.RoleId == 5)
+            {
+
+            }
         }
     }
 }
